bda0a193ac4b7c1d6e0f69fa34e29afbd02006d2
Prashant Singh <prashantsingh5aug@gmail.com>
2022-10-10 23:57:13 +0530

    Get orders api added

--- telebot/apis.py
+++ telebot/apis.py
@@ -2,10 +2,17 @@ import requests
 
 host = ""
 
-
 def getProducts():
     URL = host + "/store/products"
     r = requests.get(url = URL)
     data = r.json()
     title = data["products"]
     return title
+
+def getOrders():
+    cookie = {'connect.sid': 's%3Aa3iMHZk03QwSW6eSDYHR813XdidRpc06.gvO0cjCTakU8buuHVeoVQxsWZbfjwBDX9djtMsfrAhc'}
+    URL = host + "/admin/orders"
+    r = requests.get(url = URL,cookies=cookie)
+    data = r.json()
+    title = data["orders"]
+    return data
